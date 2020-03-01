40d85fa2dec64c57c2ea7b90c8174677077f8477
Daniel Simon <daniel.attila.simon@gmail.com>
2020-03-01 22:13:11 +0700

    Update contract addresses to newest deployment

--- packages/frontend/src/hooks/Liquity.ts
+++ packages/frontend/src/hooks/Liquity.ts
@@ -6,15 +6,15 @@ import { Liquity, LiquityContractAddresses, Trove } from "@liquity/lib";
 import { useAsyncValue } from "../hooks/AsyncValue";
 
 const contractAddresses: LiquityContractAddresses = {
-  activePool: "0xdb3977ef1bb155dBaB365c43d68C0B60b8FA3561",
-  cdpManager: "0x4333a9058f4Cb930B15b8C1610AE05329a1c0c42",
-  clvToken: "0x0Ec3E397a02412921078072888BD92Ac89744Db9",
-  defaultPool: "0xC89adD940b98844F7595f9D9EfBbB75c9B9962ea",
-  nameRegistry: "0xDEb6C3f93683A477078f47f6a3d0cbb71eb932B7",
-  poolManager: "0x591D995ed55D7b89A94adF245d902ECA95a049E4",
-  priceFeed: "0xBC89a4a5F0006bf1d936d529719B9098862B1875",
-  sortedCDPs: "0x7e20E483d1C6019862B732073982D0A9a7Fb7FBd",
-  stabilityPool: "0x1dFa9Bc2460e708D97384aF4b8910602cF5CD54D"
+  activePool: "0x04700bCA4766f391fC55A4E36da0Be83daA849F6",
+  cdpManager: "0xf9f6344919048Da7b8874780e575E087fEA009e5",
+  clvToken: "0x277A693784789582F4A154a3Eb8fd827e99B5A88",
+  defaultPool: "0xF686A081b216F818431267339B4e78E03D8282CC",
+  nameRegistry: "0x289824E4291f8c2Ab27dC1dFDFc189401B06680a",
+  poolManager: "0x581Ad97A398Ef2377a7d0c8A51Afc39Bc833Af7D",
+  priceFeed: "0x3cd61B9D6e94F2fF4D51295EA9D2D581432adA01",
+  sortedCDPs: "0x9F23490eF9A5F63546Dab89f3a6dED0Bf8467331",
+  stabilityPool: "0xCb05a079C0EbC818961866EC38B7c05827Cfc96b"
 };
 
 const LiquityContext = createContext<Liquity | undefined>(undefined);
