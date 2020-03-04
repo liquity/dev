c7738a21a73b0f204f140c21ccc7a0acd41944c6
Rick <richkws@gmail.com>
2020-03-04 22:04:07 +0000

    Remove dummy contract from migration

--- packages/contracts/migrations/2_deploy_contracts.js
+++ packages/contracts/migrations/2_deploy_contracts.js
@@ -10,7 +10,6 @@ const PriceFeed = artifacts.require("./PriceFeed.sol")
 const CLVToken = artifacts.require("./CLVToken.sol")
 const NameRegistry = artifacts.require("./NameRegistry.sol")
 const DeciMath = artifacts.require("./DeciMath.sol")
-const Blah = artifacts.require("./Blah.sol")
 const FunctionCaller = artifacts.require("./FunctionCaller.sol")
 
 const deploymentHelpers = require("../utils/deploymentHelpers.js")
@@ -33,7 +32,6 @@ module.exports = async () => {
   const defaultPool = await DefaultPool.new()
   const deciMath = await DeciMath.new()
   const functionCaller = await FunctionCaller.new()
-  const blah = await Blah.new()
 
   PriceFeed.setAsDeployed(priceFeed)
   CLVToken.setAsDeployed(clvToken)
@@ -45,7 +43,6 @@ module.exports = async () => {
   StabilityPool.setAsDeployed(stabilityPool)
   DefaultPool.setAsDeployed(defaultPool)
   DeciMath.setAsDeployed(deciMath)
-  Blah.setAsDeployed(blah)
   FunctionCaller.setAsDeployed(functionCaller)
 
   const contracts = {
