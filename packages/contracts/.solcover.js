c9f944ad285de952e6cf8363ad45d473b988936a
Daniel Simon <daniel.attila.simon@gmail.com>
2020-05-16 12:22:18 +0700

    Exclude uninteresting files from coverage

--- packages/contracts/.solcover.js
+++ packages/contracts/.solcover.js
@@ -3,5 +3,15 @@ const { accountsList: accounts } = require("./accountsList.js");
 module.exports = {
   providerOptions: {
     accounts
-  }
+  },
+
+  skipFiles: [
+    "ABDKMath64x64.sol",
+    "FunctionCaller.sol",
+    "Migrations.sol",
+    "MultiCDPGetter.sol",
+    "NameRegistry.sol",
+
+    "Interfaces"
+  ]
 };
