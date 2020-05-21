01925cb91ebc2e17e32de9bdae1c00abffc145ab
Daniel Simon <daniel.attila.simon@gmail.com>
2020-05-21 10:58:49 +0700

    Add some blank lines for readability of code

--- packages/dev-frontend/src/components/UserAccount.tsx
+++ packages/dev-frontend/src/components/UserAccount.tsx
@@ -10,22 +10,21 @@ type UserAccountProps = {
   quiBalance: Decimal;
 };
 
-export const UserAccount: React.FC<UserAccountProps> = ({ account, etherBalance, quiBalance }) => {
-  return (
-    <Flex alignItems="center">
-      <Icon name="AccountCircle" size="28px" />
-      <Box ml={3} mr={4}>
-        <Heading fontSize={1}>Connected as</Heading>
-        <Text fontSize={1}>{shortenAddress(account)}</Text>
-      </Box>
-      <Icon name="AccountBalanceWallet" size="28px" />
-      <Box ml={3}>
-        <Heading fontSize={1}>Balance</Heading>
-        <Text.span mr={3} fontSize={1}>
-          {etherBalance.prettify()} ETH
-        </Text.span>
-        <Text.span fontSize={1}>{quiBalance.prettify()} LQTY</Text.span>
-      </Box>
-    </Flex>
-  );
-};
+export const UserAccount: React.FC<UserAccountProps> = ({ account, etherBalance, quiBalance }) => (
+  <Flex alignItems="center">
+    <Icon name="AccountCircle" size="28px" />
+    <Box ml={3} mr={4}>
+      <Heading fontSize={1}>Connected as</Heading>
+      <Text fontSize={1}>{shortenAddress(account)}</Text>
+    </Box>
+
+    <Icon name="AccountBalanceWallet" size="28px" />
+    <Box ml={3}>
+      <Heading fontSize={1}>Balance</Heading>
+      <Text.span mr={3} fontSize={1}>
+        {etherBalance.prettify()} ETH
+      </Text.span>
+      <Text.span fontSize={1}>{quiBalance.prettify()} LQTY</Text.span>
+    </Box>
+  </Flex>
+);
