a3b1cc49bdb5fba12c0a4be717961937074a8e65
Daniel Simon <daniel.attila.simon@gmail.com>
2020-06-03 13:50:12 +0700

    Add a proof-of-concept for frontend testing w/ DisposableWalletProvider
    There's only one testcase and it's very simple. It's only there to prove
    that this approach can work in our CI pipeline.

--- packages/dev-frontend/src/App.test.tsx
+++ packages/dev-frontend/src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("there's no smoke", async () => {
+  const { getByText, getByTestId, findByText, queryByText } = render(<App />);
+
+  expect(await findByText(/open a new liquity trove/i)).toBeInTheDocument();
+  expect(await findByText(/there are no troves yet/i)).toBeInTheDocument();
+
+  fireEvent.click(getByTestId(/^collateral$/i));
+  fireEvent.change(getByTestId(/^collateral$/i), { target: { value: "1" } });
+  fireEvent.click(getByText(/open new trove/i));
+
+  expect(queryByText(/open new trove/i)).not.toBeInTheDocument();
+  expect(await findByText(/your liquity trove/i)).toBeInTheDocument();
 });
