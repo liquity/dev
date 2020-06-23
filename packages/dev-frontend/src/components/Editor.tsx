2931ffbd6393b124e54c2eddfe623ae2cb4ea20c
Daniel Simon <daniel.attila.simon@gmail.com>
2020-06-23 22:16:16 +0700

    Remove unused import

--- packages/dev-frontend/src/components/Editor.tsx
+++ packages/dev-frontend/src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, Flex, Label, Input } from "theme-ui";
 
 type RowProps = {
