22f7a01c1a783b8f284d3d3ea2f2f2bb4dc327b5
Daniel Simon <daniel.attila.simon@gmail.com>
2020-08-06 12:49:29 +0700

    Allow sorting Trove queries by "ICR"
    We don't actually calculate ICR itself, as that would need to be
    updated every time the price changes. Instead we simply divide
    collateral by debt, which results in the same ordering as ICR
    (collateral * price / debt) since price is the same for each Trove.
    
    A frontend can then calculate the real ICR by multiplying this
    value by the latest price.
    
    Once https://github.com/graphprotocol/graph-node/issues/880 is
    resolved, this calculation can be built into the subgraph itself.

--- packages/subgraph/src/entities/Trove.ts
+++ packages/subgraph/src/entities/Trove.ts
@@ -1,9 +1,15 @@
-import { Address, BigInt, BigDecimal } from "@graphprotocol/graph-ts";
+import { Bytes, Address, BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 
 import { getCurrentTroveOfOwner, closeCurrentTroveOfOwner } from "./Owner";
 
+// E.g. 1.5 is represented as 1.5 * 10^18, where 10^18 is called the scaling factor
 let DECIMAL_SCALING_FACTOR = BigDecimal.fromString("1000000000000000000");
-let DECIMAL_ZERO = BigDecimal.fromString("0");
+let BIGINT_SCALING_FACTOR = BigInt.fromI32(10).pow(18);
+
+let ZERO = BigInt.fromI32(0);
+let MAX_UINT256 = BigInt.fromUnsignedBytes(
+  Bytes.fromHexString("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF") as Bytes
+);
 
 export function updateTrove(
   _user: Address,
@@ -17,11 +23,20 @@ export function updateTrove(
 
   trove.collateral = _coll.divDecimal(DECIMAL_SCALING_FACTOR);
   trove.debt = _debt.divDecimal(DECIMAL_SCALING_FACTOR);
-  trove.stake = stake.divDecimal(DECIMAL_SCALING_FACTOR);
-  trove.snapshotOfTotalRedistributedCollateral = snapshotETH.divDecimal(DECIMAL_SCALING_FACTOR);
-  trove.snapshotOfTotalRedistributedDebt = snapshotCLVDebt.divDecimal(DECIMAL_SCALING_FACTOR);
 
-  if (trove.collateral.equals(DECIMAL_ZERO)) {
+  trove.rawCollateral = _coll;
+  trove.rawDebt = _debt;
+  trove.rawStake = stake;
+  trove.rawSnapshotOfTotalRedistributedCollateral = snapshotETH;
+  trove.rawSnapshotOfTotalRedistributedDebt = snapshotCLVDebt;
+
+  if (!_debt.equals(ZERO)) {
+    trove.rawCollateralPerDebt = _coll.times(BIGINT_SCALING_FACTOR).div(_debt);
+  } else {
+    trove.rawCollateralPerDebt = MAX_UINT256;
+  }
+
+  if (_coll.equals(ZERO)) {
     closeCurrentTroveOfOwner(_user);
   }
 
