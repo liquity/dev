c8934c5a10d5ebda47f61c9db04cbdfeb2d80c25
Daniel Simon <daniel.attila.simon@gmail.com>
2020-08-06 20:58:47 +0700

    Calculate virtual debt into ICR in lib

--- packages/lib/src/Liquity.ts
+++ packages/lib/src/Liquity.ts
@@ -25,10 +25,6 @@ interface Trovish {
   readonly debt?: Decimalish;
 }
 
-const calculateCollateralRatio = (collateral: Decimal, debt: Decimal, price: Decimalish) => {
-  return collateral.mulDiv(price, debt);
-};
-
 type TroveChange = {
   collateralDifference?: Difference;
   debtDifference?: Difference;
@@ -48,7 +44,7 @@ export class Trove {
   }
 
   collateralRatio(price: Decimalish): Decimal {
-    return calculateCollateralRatio(this.collateral, this.debt, price);
+    return this.collateral.mulDiv(price, this.debt.add(Liquity.VIRTUAL_DEBT));
   }
 
   collateralRatioIsBelowMinimum(price: Decimalish) {
@@ -313,6 +309,7 @@ const numberify = (bigNumber: BigNumber) => bigNumber.toNumber();
 export class Liquity {
   public static readonly CRITICAL_COLLATERAL_RATIO: Decimal = Decimal.from(1.5);
   public static readonly MINIMUM_COLLATERAL_RATIO: Decimal = Decimal.from(1.1);
+  public static readonly VIRTUAL_DEBT: Decimal = Decimal.from(10);
 
   public static useHint = true;
 
