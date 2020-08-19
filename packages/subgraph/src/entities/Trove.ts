8ef8a3b4837bb194d5968cc63395103eb205864d
Daniel Simon <daniel.attila.simon@gmail.com>
2020-08-19 15:55:08 +0700

    Save price in TroveChanges
    This also allows calculating what the ICR was before and after the
    change. With this, one can e.g. see at what ICR liquidations happen.

--- packages/subgraph/src/entities/Trove.ts
+++ packages/subgraph/src/entities/Trove.ts
@@ -1,4 +1,4 @@
-import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts";
+import { ethereum, Address, BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 
 import { TroveChange } from "../../generated/schema";
 
@@ -6,10 +6,11 @@ import {
   BIGINT_SCALING_FACTOR,
   DECIMAL_SCALING_FACTOR,
   BIGINT_ZERO,
-  BIGINT_MAX_UINT256
+  BIGINT_MAX_UINT256,
+  DECIMAL_ZERO
 } from "../utils/bignumbers";
 
-import { getChangeId, initChange } from "./System";
+import { getChangeId, initChange, getCurrentPrice } from "./System";
 import { getCurrentTroveOfOwner, closeCurrentTroveOfOwner } from "./Owner";
 
 function createTroveChange(event: ethereum.Event): TroveChange {
@@ -17,6 +18,18 @@ function createTroveChange(event: ethereum.Event): TroveChange {
   initChange(troveChange, event);
 
   return troveChange;
+}
+
+function calculateCollateralRatio(
+  collateral: BigDecimal,
+  debt: BigDecimal,
+  price: BigDecimal
+): BigDecimal | null {
+  if (debt.equals(DECIMAL_ZERO)) {
+    return null;
+  }
+
+  return collateral.times(price).div(debt);
 }
 
 export function updateTrove(
@@ -30,21 +43,27 @@ export function updateTrove(
   snapshotCLVDebt: BigInt
 ): void {
   let trove = getCurrentTroveOfOwner(_user);
-
+  let price = getCurrentPrice();
   let troveChange = createTroveChange(event);
+
   troveChange.trove = trove.id;
   troveChange.troveOperation = operation;
+  troveChange.price = price;
 
   troveChange.collateralBefore = trove.collateral;
   troveChange.debtBefore = trove.debt;
+  troveChange.collateralRatioBefore = calculateCollateralRatio(trove.collateral, trove.debt, price);
 
   trove.collateral = _coll.divDecimal(DECIMAL_SCALING_FACTOR);
   trove.debt = _debt.divDecimal(DECIMAL_SCALING_FACTOR);
 
   troveChange.collateralAfter = trove.collateral;
-  troveChange.collateralChange = troveChange.collateralAfter.minus(troveChange.collateralBefore);
   troveChange.debtAfter = trove.debt;
+  troveChange.collateralRatioAfter = calculateCollateralRatio(trove.collateral, trove.debt, price);
+
+  troveChange.collateralChange = troveChange.collateralAfter.minus(troveChange.collateralBefore);
   troveChange.debtChange = troveChange.debtAfter.minus(troveChange.debtBefore);
+
   troveChange.save();
 
   trove.rawCollateral = _coll;
