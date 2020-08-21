995b1a5c37ef98120eb1927857c86bd91b2d6ea5
Daniel Simon <daniel.attila.simon@gmail.com>
2020-08-21 12:28:41 +0700

    Filter out pointless StabilityDepositChanges

--- packages/subgraph/src/entities/StabilityDeposit.ts
+++ packages/subgraph/src/entities/StabilityDeposit.ts
@@ -2,7 +2,7 @@ import { ethereum, Address, BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 
 import { StabilityDepositChange, StabilityDeposit } from "../../generated/schema";
 
-import { DECIMAL_SCALING_FACTOR, DECIMAL_ZERO } from "../utils/bignumbers";
+import { DECIMAL_SCALING_FACTOR, DECIMAL_ZERO, BIGINT_ZERO } from "../utils/bignumbers";
 
 import { getChangeId, initChange } from "./System";
 import { getCurrentStabilityDepositOfOwner, closeCurrentStabilityDepositOfOwner } from "./Owner";
@@ -75,6 +75,11 @@ export function withdrawCollateralGainFromStabilityDeposit(
   _ETH: BigInt,
   _CLVLoss: BigInt
 ): void {
+  if (_ETH.equals(BIGINT_ZERO) && _CLVLoss.equals(BIGINT_ZERO)) {
+    // Ignore "NOP" event
+    return;
+  }
+
   let stabilityDeposit = getCurrentStabilityDepositOfOwner(_user);
   let newDepositedAmount = stabilityDeposit.depositedAmount.minus(
     _CLVLoss.divDecimal(DECIMAL_SCALING_FACTOR)
