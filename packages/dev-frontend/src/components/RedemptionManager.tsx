f230dbe781123895b15a7c4545de3ca1f866c5af
ßingen <bingen@liquity.org>
2020-11-06 17:17:14 +0100

    Revert "dev-frontend: Add maxIterations to redeemCollateral"
    This reverts commit e5df3f6d73fdff66acdc71c9b569b5832b084abc.

--- packages/dev-frontend/src/components/RedemptionManager.tsx
+++ packages/dev-frontend/src/components/RedemptionManager.tsx
@@ -56,7 +56,7 @@ const RedemptionAction: React.FC<RedemptionActionProps> = ({
     return null;
   }
 
-  const send = liquity.redeemCollateral.bind(liquity, exchangedQui, { price, numberOfTroves }, 0);
+  const send = liquity.redeemCollateral.bind(liquity, exchangedQui, { price, numberOfTroves });
 
   return myTransactionState.type === "waitingForApproval" ? (
     <Flex variant="layout.actions">
