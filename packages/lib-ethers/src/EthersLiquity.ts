d7a676e6bc33439ab02569e072692767ad0a9695
ßingen <bingen@liquity.org>
2020-11-18 18:54:41 +0100

    fixup! fixup! lib-ethers: Remove PoolManager

--- packages/lib-ethers/src/EthersLiquity.ts
+++ packages/lib-ethers/src/EthersLiquity.ts
@@ -60,7 +60,7 @@ enum CDPManagerOperation {
 
 const debouncingDelayMs = 50;
 // With 85 iterations redemption costs about ~10M gas, and each iteration accounts for ~118,500 more
-export const redeemMaxIterations = 85;
+export const redeemMaxIterations = 86;
 
 const debounce = (listener: (latestBlock: number) => void) => {
   let timeoutId: any = undefined;
