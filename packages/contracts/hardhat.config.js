a76b6df58b23c3e4cc7e802a2f47a1c4811fe908
ßingen <bingen@liquity.org>
2020-11-19 16:48:25 +0100

    fixup! contracts: wip: Upgrade to hardhat

---
+++ packages/contracts/hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-ethers");
+//require("solidity-coverage");
+require("hardhat-gas-reporter");
+
+const accounts = require("./buidlerAccountsList2k.js");
+
+const accountsList = accounts.accountsList
+
+module.exports = {
+    paths: {
+        // contracts: "./contracts",
+        // artifacts: "./artifacts"
+    },
+    solidity: {
+        version: "0.6.11",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 100
+            }
+        }
+    },
+    networks: {
+        hardhat: {
+            accounts: accountsList,
+            gas: 10000000,  // tx gas limit
+            blockGasLimit: 10000000, 
+            gasPrice: 20000000000
+        }
+    },
+    mocha: { timeout: 12000000 },
+    rpc: {
+        host: "localhost",
+        port: 8545
+    },
+    gasReporter: {
+        enabled: (process.env.REPORT_GAS) ? true : false
+    }
+};
