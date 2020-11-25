2f212d0f7974b90b8bb221c778f5e8e9380f6a65
ßingen <bingen@liquity.org>
2020-11-25 18:54:22 +0100

    contracts: Cosmetic change to console contract

--- packages/contracts/contracts/Dependencies/console.sol
+++ packages/contracts/contracts/Dependencies/console.sol
@@ -9,7 +9,9 @@ library console {
 	function log() internal view {
 		(bool ignored, ) = CONSOLE_ADDRESS.staticcall(abi.encodeWithSignature("log()"));
 		ignored;
-	}	function logInt(int p0) internal view {
+	}
+
+	function logInt(int p0) internal view {
 		(bool ignored, ) = CONSOLE_ADDRESS.staticcall(abi.encodeWithSignature("log(int)", p0));
 		ignored;
 	}
