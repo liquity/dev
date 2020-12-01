e2618fc686dc25c5c9bbf0be1825d803848a1a07
ßingen <bingen@liquity.org>
2020-12-02 00:13:49 +0100

    contracts: Enhance NonPayable mock contract

--- packages/contracts/contracts/TestContracts/NonPayable.sol
+++ packages/contracts/contracts/TestContracts/NonPayable.sol
@@ -2,14 +2,23 @@
 
 pragma solidity 0.6.11;
 
+//import "../Dependencies/console.sol";
+
 
 contract NonPayable {
+    bool isPayable;
+
+    function setPayable(bool _isPayable) external {
+        isPayable = _isPayable;
+    }
+
     function forward(address _dest, bytes calldata _data) external payable {
         (bool success, bytes memory returnData) = _dest.call{ value: msg.value }(_data);
+        //console.logBytes(returnData);
         require(success, string(returnData));
     }
 
     receive() external payable {
-        revert();
+        require(isPayable);
     }
 }
