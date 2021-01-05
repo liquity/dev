e4404cea0a77472897a86c94016da783db292032
Daniel Simon <daniel.attila.simon@gmail.com>
2021-01-05 19:43:51 +0700

    dev-frontend: Implement collateral surplus claiming
    Resolves #182.

--- packages/dev-frontend/src/components/LoadingOverlay.tsx
+++ packages/dev-frontend/src/components/LoadingOverlay.tsx
@@ -3,7 +3,7 @@ import { Container, Spinner } from "theme-ui";
 
 export const LoadingOverlay: React.FC = () => (
   <Container
-    variant="loadingOverlay"
+    variant="disabledOverlay"
     sx={{ p: "14px", display: "flex", justifyContent: "flex-end" }}
   >
     <Spinner size="28px" sx={{ color: "text" }} />
