244348d200c0b6bc6b78ef87e75b56524d1d05a8
ßingen <bingen@liquity.org>
2021-01-15 11:47:20 +0100

    contracts: Make TYPE_HASH constant in both tokens

--- packages/contracts/contracts/LUSDToken.sol
+++ packages/contracts/contracts/LUSDToken.sol
@@ -36,7 +36,8 @@ contract LUSDToken is ILUSDToken {
     
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 private constant _PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-    bytes32 private immutable _TYPE_HASH;
+    // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
+    bytes32 private constant _TYPE_HASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
 
     // Cache the domain separator as an immutable value, but also store the chain id that it corresponds to, in order to
     // invalidate the cached domain separator if the chain id changes.
@@ -77,14 +78,10 @@ contract LUSDToken is ILUSDToken {
         bytes32 hashedName = keccak256(bytes(_NAME));
         bytes32 hashedVersion = keccak256(bytes(_VERSION));
         
-        // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"); 
-        bytes32 typeHash = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
-        
-        _TYPE_HASH = typeHash;
         _HASHED_NAME = hashedName;
         _HASHED_VERSION = hashedVersion;
         _CACHED_CHAIN_ID = _chainID();
-        _CACHED_DOMAIN_SEPARATOR = _buildDomainSeparator(typeHash, hashedName, hashedVersion);
+        _CACHED_DOMAIN_SEPARATOR = _buildDomainSeparator(_TYPE_HASH, hashedName, hashedVersion);
     }
 
     // --- Functions for intra-Liquity calls ---
