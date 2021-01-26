a1bb3660705953b88056b37bd7a4dae4487e804a
ßingen <bingen@liquity.org>
2021-01-26 18:48:12 +0100

    contracts: Make Unipool compile
    - Upgrade to solidity 0.6.11
    - Get rid of IRewardDistributionRecipient (and replace it by
      LQTYToken)

--- packages/contracts/contracts/LPRewards/Unipool.sol
+++ packages/contracts/contracts/LPRewards/Unipool.sol
@@ -1,10 +1,11 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/math/Math.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "./IRewardDistributionRecipient.sol";
+pragma solidity 0.6.11;
+
+import "../Dependencies/LiquityMath.sol";
+import "../Dependencies/SafeMath.sol";
+import "./SafeERC20.sol";
+
 
 contract LPTokenWrapper {
     using SafeMath for uint256;
@@ -23,22 +24,23 @@ contract LPTokenWrapper {
         return _balances[account];
     }
 
-    function stake(uint256 amount) public {
+    function stake(uint256 amount) public virtual {
         _totalSupply = _totalSupply.add(amount);
         _balances[msg.sender] = _balances[msg.sender].add(amount);
         uni.safeTransferFrom(msg.sender, address(this), amount);
     }
 
-    function withdraw(uint256 amount) public {
+    function withdraw(uint256 amount) public virtual {
         _totalSupply = _totalSupply.sub(amount);
         _balances[msg.sender] = _balances[msg.sender].sub(amount);
         uni.safeTransfer(msg.sender, amount);
     }
 }
 
-contract Unipool is LPTokenWrapper, IRewardDistributionRecipient {
+contract Unipool is LPTokenWrapper {
     IERC20 public snx = IERC20(0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F);
     uint256 public constant DURATION = 7 days;
+    address _lqtyToken;
 
     uint256 public periodFinish = 0;
     uint256 public rewardRate = 0;
@@ -63,7 +65,7 @@ contract Unipool is LPTokenWrapper, IRewardDistributionRecipient {
     }
 
     function lastTimeRewardApplicable() public view returns (uint256) {
-        return Math.min(block.timestamp, periodFinish);
+        return LiquityMath._min(block.timestamp, periodFinish);
     }
 
     function rewardPerToken() public view returns (uint256) {
@@ -89,13 +91,13 @@ contract Unipool is LPTokenWrapper, IRewardDistributionRecipient {
     }
 
     // stake visibility is public as overriding LPTokenWrapper's stake() function
-    function stake(uint256 amount) public updateReward(msg.sender) {
+    function stake(uint256 amount) public override updateReward(msg.sender) {
         require(amount > 0, "Cannot stake 0");
         super.stake(amount);
         emit Staked(msg.sender, amount);
     }
 
-    function withdraw(uint256 amount) public updateReward(msg.sender) {
+    function withdraw(uint256 amount) public override updateReward(msg.sender) {
         require(amount > 0, "Cannot withdraw 0");
         super.withdraw(amount);
         emit Withdrawn(msg.sender, amount);
@@ -117,9 +119,10 @@ contract Unipool is LPTokenWrapper, IRewardDistributionRecipient {
 
     function notifyRewardAmount(uint256 reward)
         external
-        onlyRewardDistribution
         updateReward(address(0))
     {
+        _requireCallerIsLQTYToken();
+
         if (block.timestamp >= periodFinish) {
             rewardRate = reward.div(DURATION);
         } else {
@@ -130,5 +133,9 @@ contract Unipool is LPTokenWrapper, IRewardDistributionRecipient {
         lastUpdateTime = block.timestamp;
         periodFinish = block.timestamp.add(DURATION);
         emit RewardAdded(reward);
+    }
+
+    function _requireCallerIsLQTYToken() internal view {
+        require(msg.sender == _lqtyToken, "Unipool: Caller must be the LQTY token");
     }
 }
