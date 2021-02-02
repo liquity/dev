b30a59f49e9534c7bf20123e5976688bddcd6454
Daniel Simon <daniel.attila.simon@gmail.com>
2021-02-02 16:25:29 +0700

    lib-base: Fix Trove tests after default borrowing rate change
    When the borrowing rate is non-0, not all Troves can be recreated
    with 100% accuracy.

--- packages/lib-base/test/Trove.test.ts
+++ packages/lib-base/test/Trove.test.ts
@@ -2,7 +2,7 @@ import assert from "assert";
 import { describe, it } from "mocha";
 import fc from "fast-check";
 
-import { Decimal } from "@liquity/decimal";
+import { Decimal, Difference } from "@liquity/decimal";
 
 import { LUSD_LIQUIDATION_RESERVE } from "../src/constants";
 import { Trove, _emptyTrove } from "../src/Trove";
@@ -29,6 +29,9 @@ const validTrove = () =>
 
 const validNonEmptyTrove = () => validTrove().filter(t => !t.isEmpty);
 
+const roughlyEqual = (a: Trove, b: Trove) =>
+  a.collateral.eq(b.collateral) && !!Difference.between(a.debt, b.debt).absoluteValue?.lt(1e-9);
+
 describe("Trove", () => {
   it("applying undefined diff should yield the same Trove", () => {
     const trove = new Trove(Decimal.from(1), Decimal.from(111));
@@ -36,7 +39,7 @@ describe("Trove", () => {
     assert(trove.apply(undefined) === trove);
   });
 
-  it("applying diff of empty from `b` to `a` should always yield empty", () => {
+  it("applying diff of empty from `b` to `a` should yield empty", () => {
     fc.assert(
       fc.property(validNonEmptyTrove(), validNonEmptyTrove(), (a, b) =>
         a.apply(b.whatChanged(_emptyTrove)).equals(_emptyTrove)
@@ -64,9 +67,19 @@ describe("Trove", () => {
     );
   });
 
-  it("applying diff of `b` from `a` to `a` should always yield `b`", () => {
+  it("applying diff of `b` from `a` to `a` should yield `b` when borrowing rate is 0", () => {
     fc.assert(
-      fc.property(validTrove(), arbitraryTrove(), (a, b) => a.apply(a.whatChanged(b)).equals(b))
+      fc.property(validTrove(), arbitraryTrove(), (a, b) =>
+        a.apply(a.whatChanged(b, 0), 0).equals(b)
+      )
+    );
+  });
+
+  it("applying diff of `b` from `a` to `a` should roughly yield `b` when borrowing rate is non-0", () => {
+    fc.assert(
+      fc.property(validTrove(), arbitraryTrove(), fc.float({ max: 0.5 }), (a, b, c) =>
+        roughlyEqual(a.apply(a.whatChanged(b, c), c), b)
+      )
     );
   });
 
