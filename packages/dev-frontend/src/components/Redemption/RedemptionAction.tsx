fceaf80b4b38ec3286fba978ead0325af2824fc2
Daniel Simon <daniel.attila.simon@gmail.com>
2021-03-29 18:46:35 +0700

    fix: fix some panels not transitioning to read-only after TX success

--- packages/dev-frontend/src/components/Redemption/RedemptionAction.tsx
+++ packages/dev-frontend/src/components/Redemption/RedemptionAction.tsx
@@ -1,48 +1,31 @@
-import React, { useEffect } from "react";
 import { Button } from "theme-ui";
 
 import { Decimal } from "@liquity/lib-base";
 
 import { useLiquity } from "../../hooks/LiquityContext";
-import { useMyTransactionState, useTransactionFunction } from "../Transaction";
+import { useTransactionFunction } from "../Transaction";
 
 type RedemptionActionProps = {
+  transactionId: string;
   disabled?: boolean;
   lusdAmount: Decimal;
-  setLUSDAmount: (lusdAmount: Decimal) => void;
-  setChangePending: (isPending: boolean) => void;
   maxRedemptionRate: Decimal;
 };
 
 export const RedemptionAction: React.FC<RedemptionActionProps> = ({
+  transactionId,
   disabled,
   lusdAmount,
-  setLUSDAmount,
-  setChangePending,
   maxRedemptionRate
 }) => {
   const {
     liquity: { send: liquity }
   } = useLiquity();
 
-  const myTransactionId = "redemption";
-  const myTransactionState = useMyTransactionState(myTransactionId);
-
   const [sendTransaction] = useTransactionFunction(
-    myTransactionId,
+    transactionId,
     liquity.redeemLUSD.bind(liquity, lusdAmount, maxRedemptionRate)
   );
-
-  useEffect(() => {
-    if (myTransactionState.type === "waitingForApproval") {
-      setChangePending(true);
-    } else if (myTransactionState.type === "failed" || myTransactionState.type === "cancelled") {
-      setChangePending(false);
-    } else if (myTransactionState.type === "confirmed") {
-      setLUSDAmount(Decimal.ZERO);
-      setChangePending(false);
-    }
-  }, [myTransactionState.type, setChangePending, setLUSDAmount]);
 
   return (
     <Button disabled={disabled} onClick={sendTransaction}>
