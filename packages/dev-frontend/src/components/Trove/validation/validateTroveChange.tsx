e913ef9369d6491f1258dd88e60a09ca16fe93ad
Daniel Simon <daniel.attila.simon@gmail.com>
2021-03-31 18:43:29 +0700

    fix: match Trove pre-flight checks to contracts
    Fixes #407.

--- packages/dev-frontend/src/components/Trove/validation/validateTroveChange.tsx
+++ packages/dev-frontend/src/components/Trove/validation/validateTroveChange.tsx
@@ -7,7 +7,9 @@ import {
   Percent,
   MINIMUM_COLLATERAL_RATIO,
   CRITICAL_COLLATERAL_RATIO,
-  LiquityStoreState
+  LiquityStoreState,
+  TroveClosureParams,
+  TroveCreationParams
 } from "@liquity/lib-base";
 
 import { COIN } from "../../../strings";
@@ -22,7 +24,7 @@ type TroveAdjustmentDescriptionParams = {
   params: TroveAdjustmentParams<Decimal>;
 };
 
-const TroveAdjustmentDescription: React.FC<TroveAdjustmentDescriptionParams> = ({ params }) => (
+const TroveChangeDescription: React.FC<TroveAdjustmentDescriptionParams> = ({ params }) => (
   <ActionDescription>
     {params.depositCollateral && params.borrowLUSD ? (
       <>
@@ -88,34 +90,54 @@ export const selectForTroveChangeValidation = ({
   numberOfTroves
 }: LiquityStoreState) => ({ price, total, accountBalance, lusdBalance, numberOfTroves });
 
-type TroveChangeValidationContext = ReturnType<typeof selectForTroveChangeValidation>;
+type TroveChangeValidationSelectedState = ReturnType<typeof selectForTroveChangeValidation>;
+
+interface TroveChangeValidationContext extends TroveChangeValidationSelectedState {
+  originalTrove: Trove;
+  resultingTrove: Trove;
+  recoveryMode: boolean;
+  wouldTriggerRecoveryMode: boolean;
+}
 
 export const validateTroveChange = (
-  original: Trove,
-  edited: Trove,
+  originalTrove: Trove,
+  adjustedTrove: Trove,
   borrowingRate: Decimal,
-  { price, total, accountBalance, lusdBalance, numberOfTroves }: TroveChangeValidationContext
+  selectedState: TroveChangeValidationSelectedState
 ): [
   validChange: Exclude<TroveChange<Decimal>, { type: "invalidCreation" }> | undefined,
   description: JSX.Element | undefined
 ] => {
-  const change = original.whatChanged(edited, borrowingRate);
-  // Reapply change to get the exact state the Trove will end up in (which could be slightly
-  // different from `edited` due to imprecision).
-  const afterFee = original.apply(change, borrowingRate);
+  const { total, price } = selectedState;
+  const change = originalTrove.whatChanged(adjustedTrove, borrowingRate);
 
   if (!change) {
     return [undefined, undefined];
   }
 
-  if (
-    change.type === "invalidCreation" ||
-    (change.type !== "closure" && afterFee.debt.lt(LUSD_MINIMUM_DEBT))
-  ) {
+  // Reapply change to get the exact state the Trove will end up in (which could be slightly
+  // different from `edited` due to imprecision).
+  const resultingTrove = originalTrove.apply(change, borrowingRate);
+  const recoveryMode = total.collateralRatioIsBelowCritical(price);
+  const wouldTriggerRecoveryMode = total
+    .subtract(originalTrove)
+    .add(resultingTrove)
+    .collateralRatioIsBelowCritical(price);
+
+  const context: TroveChangeValidationContext = {
+    ...selectedState,
+    originalTrove,
+    resultingTrove,
+    recoveryMode,
+    wouldTriggerRecoveryMode
+  };
+
+  if (change.type === "invalidCreation") {
+    // Trying to create a Trove with negative net debt
     return [
       undefined,
       <ErrorDescription>
-        Debt must be be at least{" "}
+        Debt must be at least{" "}
         <Amount>
           {LUSD_MINIMUM_DEBT.toString()} {COIN}
         </Amount>
@@ -124,118 +146,223 @@ export const validateTroveChange = (
     ];
   }
 
-  if (
-    (change.type === "creation" ||
-      (change.type === "adjustment" &&
-        (change.params.withdrawCollateral || change.params.borrowLUSD))) &&
-    afterFee.collateralRatioIsBelowMinimum(price)
-  ) {
-    return [
-      undefined,
-      <ErrorDescription>
-        Collateral ratio must be at least <Amount>{mcrPercent}</Amount>.
-      </ErrorDescription>
-    ];
+  const errorDescription =
+    change.type === "creation"
+      ? validateTroveCreation(change.params, context)
+      : change.type === "closure"
+      ? validateTroveClosure(change.params, context)
+      : validateTroveAdjustment(change.params, context);
+
+  if (errorDescription) {
+    return [undefined, errorDescription];
   }
 
-  if (
-    (change.type === "creation" || change.type === "adjustment") &&
-    !total.collateralRatioIsBelowCritical(price) &&
-    total.subtract(original).add(afterFee).collateralRatioIsBelowCritical(price)
-  ) {
-    return [
-      undefined,
-      change.type === "creation" ? (
+  return [change, <TroveChangeDescription params={change.params} />];
+};
+
+const validateTroveCreation = (
+  { depositCollateral }: TroveCreationParams<Decimal>,
+  {
+    resultingTrove,
+    recoveryMode,
+    wouldTriggerRecoveryMode,
+    accountBalance,
+    price
+  }: TroveChangeValidationContext
+): JSX.Element | null => {
+  if (resultingTrove.debt.lt(LUSD_MINIMUM_DEBT)) {
+    return (
+      <ErrorDescription>
+        Debt must be at least{" "}
+        <Amount>
+          {LUSD_MINIMUM_DEBT.toString()} {COIN}
+        </Amount>
+        .
+      </ErrorDescription>
+    );
+  }
+
+  if (recoveryMode) {
+    if (!resultingTrove.isOpenableInRecoveryMode(price)) {
+      return (
+        <ErrorDescription>
+          You're not allowed to open a Trove with less than <Amount>{ccrPercent}</Amount> collateral
+          ratio during recovery mode. Please increase your collateral ratio.
+        </ErrorDescription>
+      );
+    }
+  } else {
+    if (resultingTrove.collateralRatioIsBelowMinimum(price)) {
+      return (
+        <ErrorDescription>
+          Collateral ratio must be at least <Amount>{mcrPercent}</Amount>.
+        </ErrorDescription>
+      );
+    }
+
+    if (wouldTriggerRecoveryMode) {
+      return (
         <ErrorDescription>
           You're not allowed to open a Trove that would cause the total collateral ratio to fall
           below <Amount>{ccrPercent}</Amount>. Please increase your collateral ratio.
         </ErrorDescription>
-      ) : (
+      );
+    }
+  }
+
+  if (depositCollateral.gt(accountBalance)) {
+    return (
+      <ErrorDescription>
+        The amount you're trying to deposit exceeds your balance by{" "}
+        <Amount>{depositCollateral.sub(accountBalance).prettify()} ETH</Amount>.
+      </ErrorDescription>
+    );
+  }
+
+  return null;
+};
+
+const validateTroveAdjustment = (
+  { depositCollateral, withdrawCollateral, borrowLUSD, repayLUSD }: TroveAdjustmentParams<Decimal>,
+  {
+    originalTrove,
+    resultingTrove,
+    recoveryMode,
+    wouldTriggerRecoveryMode,
+    price,
+    accountBalance,
+    lusdBalance
+  }: TroveChangeValidationContext
+): JSX.Element | null => {
+  if (recoveryMode) {
+    if (withdrawCollateral) {
+      return (
+        <ErrorDescription>
+          You're not allowed to withdraw collateral during recovery mode.
+        </ErrorDescription>
+      );
+    }
+
+    if (borrowLUSD) {
+      if (resultingTrove.collateralRatioIsBelowCritical(price)) {
+        return (
+          <ErrorDescription>
+            Your collateral ratio must be at least <Amount>{ccrPercent}</Amount> to borrow during
+            recovery mode. Please improve your collateral ratio.
+          </ErrorDescription>
+        );
+      }
+
+      if (resultingTrove.collateralRatio(price).lt(originalTrove.collateralRatio(price))) {
+        return (
+          <ErrorDescription>
+            You're not allowed to decrease your collateral ratio during recovery mode.
+          </ErrorDescription>
+        );
+      }
+    }
+  } else {
+    if (resultingTrove.collateralRatioIsBelowMinimum(price)) {
+      return (
+        <ErrorDescription>
+          Collateral ratio must be at least <Amount>{mcrPercent}</Amount>.
+        </ErrorDescription>
+      );
+    }
+
+    if (wouldTriggerRecoveryMode) {
+      return (
         <ErrorDescription>
           The adjustment you're trying to make would cause the total collateral ratio to fall below{" "}
           <Amount>{ccrPercent}</Amount>. Please increase your collateral ratio.
         </ErrorDescription>
-      )
-    ];
+      );
+    }
   }
 
-  if (change.params.depositCollateral?.gt(accountBalance)) {
-    return [
-      undefined,
-      <ErrorDescription>
-        The amount you're trying to deposit exceeds your balance by{" "}
-        <Amount>{change.params.depositCollateral.sub(accountBalance).prettify()} ETH</Amount>.
-      </ErrorDescription>
-    ];
-  }
-
-  if (change.params.repayLUSD?.gt(lusdBalance)) {
-    return [
-      undefined,
-      edited.isEmpty ? (
+  if (repayLUSD) {
+    if (resultingTrove.debt.lt(LUSD_MINIMUM_DEBT)) {
+      return (
         <ErrorDescription>
-          You need{" "}
+          Debt must be at least{" "}
           <Amount>
-            {change.params.repayLUSD.sub(lusdBalance).prettify()} {COIN}
-          </Amount>{" "}
-          more to close your Trove.
-        </ErrorDescription>
-      ) : (
-        <ErrorDescription>
-          The amount you're trying to repay exceeds your balance by{" "}
-          <Amount>
-            {change.params.repayLUSD.sub(lusdBalance).prettify()} {COIN}
+            {LUSD_MINIMUM_DEBT.toString()} {COIN}
           </Amount>
           .
         </ErrorDescription>
-      )
-    ];
+      );
+    }
+
+    if (repayLUSD.gt(lusdBalance)) {
+      return (
+        <ErrorDescription>
+          The amount you're trying to repay exceeds your balance by{" "}
+          <Amount>
+            {repayLUSD.sub(lusdBalance).prettify()} {COIN}
+          </Amount>
+          .
+        </ErrorDescription>
+      );
+    }
   }
 
-  if (
-    change.type === "creation" &&
-    total.collateralRatioIsBelowCritical(price) &&
-    !afterFee.isOpenableInRecoveryMode(price)
-  ) {
-    return [
-      undefined,
+  if (depositCollateral?.gt(accountBalance)) {
+    return (
       <ErrorDescription>
-        You're not allowed to open a Trove with less than <Amount>{ccrPercent}</Amount> collateral
-        ratio during recovery mode. Please increase your collateral ratio.
+        The amount you're trying to deposit exceeds your balance by{" "}
+        <Amount>{depositCollateral.sub(accountBalance).prettify()} ETH</Amount>.
       </ErrorDescription>
-    ];
+    );
   }
 
-  if (change.type === "closure" && total.collateralRatioIsBelowCritical(price)) {
-    return [
-      undefined,
-      <ErrorDescription>
-        You're not allowed to close your Trove during recovery mode.
-      </ErrorDescription>
-    ];
-  }
+  return null;
+};
 
-  if (change.type === "closure" && numberOfTroves === 1) {
-    return [
-      undefined,
+const validateTroveClosure = (
+  { repayLUSD }: TroveClosureParams<Decimal>,
+  {
+    recoveryMode,
+    wouldTriggerRecoveryMode,
+    numberOfTroves,
+    lusdBalance
+  }: TroveChangeValidationContext
+): JSX.Element | null => {
+  if (numberOfTroves === 1) {
+    return (
       <ErrorDescription>
         You're not allowed to close your Trove when there are no other Troves in the system.
       </ErrorDescription>
-    ];
+    );
   }
 
-  if (
-    change.type === "adjustment" &&
-    total.collateralRatioIsBelowCritical(price) &&
-    afterFee.collateralRatio(price).lt(original.collateralRatio(price))
-  ) {
-    return [
-      undefined,
+  if (recoveryMode) {
+    return (
       <ErrorDescription>
-        You're not allowed to decrease your collateral ratio during recovery mode.
+        You're not allowed to close your Trove during recovery mode.
       </ErrorDescription>
-    ];
+    );
   }
 
-  return [change, <TroveAdjustmentDescription params={change.params} />];
+  if (repayLUSD?.gt(lusdBalance)) {
+    return (
+      <ErrorDescription>
+        You need{" "}
+        <Amount>
+          {repayLUSD.sub(lusdBalance).prettify()} {COIN}
+        </Amount>{" "}
+        more to close your Trove.
+      </ErrorDescription>
+    );
+  }
+
+  if (wouldTriggerRecoveryMode) {
+    return (
+      <ErrorDescription>
+        Closing a Trove is not allowed if it would cause the total collateral ratio to fall below{" "}
+        <Amount>{ccrPercent}</Amount>. Please wait until the total collateral ratio increases.
+      </ErrorDescription>
+    );
+  }
+
+  return null;
 };
