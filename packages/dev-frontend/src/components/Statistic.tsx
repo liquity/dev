f609bb1490f0e8f4d10c41a57c26bf1dfbde2994
Edward Mulraney <edmulraney@gmail.com>
2021-04-04 11:44:47 +0100

    make account balances use new style

--- packages/dev-frontend/src/components/Statistic.tsx
+++ packages/dev-frontend/src/components/Statistic.tsx
@@ -4,7 +4,7 @@ import { InfoIcon } from "./InfoIcon";
 
 type StatisticProps = {
   name: React.ReactNode;
-  tooltip: React.ReactNode;
+  tooltip?: React.ReactNode;
 };
 
 export const Statistic: React.FC<StatisticProps> = ({ name, tooltip, children }) => {
@@ -12,7 +12,7 @@ export const Statistic: React.FC<StatisticProps> = ({ name, tooltip, children })
     <Flex sx={{ borderBottom: 1, borderColor: "rgba(0, 0, 0, 0.1)" }}>
       <Flex sx={{ alignItems: "center", justifyContent: "flex-start", flex: 1.2, fontWeight: 200 }}>
         <Flex>{name}</Flex>
-        <InfoIcon size="xs" tooltip={<Card variant="tooltip">{tooltip}</Card>} />
+        {tooltip && <InfoIcon size="xs" tooltip={<Card variant="tooltip">{tooltip}</Card>} />}
       </Flex>
       <Flex sx={{ justifyContent: "flex-start", flex: 0.8, alignItems: "center" }}>{children}</Flex>
     </Flex>
