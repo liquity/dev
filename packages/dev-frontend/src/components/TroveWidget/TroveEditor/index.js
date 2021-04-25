e9b2f80d74b1f66137968ef691ffafbbd4976887
lukabajic <lukabajic.dev@gmail.com>
2021-04-25 23:36:18 +0200

    Add withdraw and deposit

--- packages/dev-frontend/src/components/TroveWidget/TroveEditor/index.js
+++ packages/dev-frontend/src/components/TroveWidget/TroveEditor/index.js
@@ -1,21 +1,36 @@
-import { useState } from "react";
-import { Heading, Box, Card, Button } from "theme-ui";
+import { useState, useEffect } from "react";
 
-import { Percent, Difference, Decimal, LUSD_LIQUIDATION_RESERVE } from "@liquity/lib-base";
+import {
+  Percent,
+  Decimal,
+  LUSD_LIQUIDATION_RESERVE,
+  CRITICAL_COLLATERAL_RATIO,
+  LUSD_MINIMUM_DEBT
+} from "@liquity/lib-base";
 import { useLiquitySelector } from "@liquity/lib-react";
 
-import { COIN } from "../../../strings";
-
-import { Icon } from "../../Icon";
-import { EditableRow, StaticRow } from "../../Trove/Editor";
 import { LoadingOverlay } from "../../LoadingOverlay";
-import { InfoIcon } from "../../InfoIcon";
+import Input from "../../Input";
+import StaticRow from "../../StaticRow";
+
+import { ETH, COIN } from "../../../strings";
+
+import classes from "./TroveEditor.module.css";
 
 const gasRoomETH = Decimal.from(0.1);
 
+const getColor = ratio =>
+  ratio?.gt(CRITICAL_COLLATERAL_RATIO)
+    ? "success"
+    : ratio?.gt(1.2)
+    ? "warning"
+    : ratio?.lte(1.2)
+    ? "danger"
+    : "muted";
+
 const select = ({ price, accountBalance }) => ({ price, accountBalance });
 
-const TroveEditor = ({
+export const TroveDeposit = ({
   children,
   original,
   edited,
@@ -25,105 +40,228 @@ const TroveEditor = ({
   dispatch
 }) => {
   const { price, accountBalance } = useLiquitySelector(select);
-
-  const editingState = useState();
+  const [deposit, setDeposit] = useState("");
+  const [borrow, setBorrow] = useState("");
 
   const feePct = new Percent(borrowingRate);
 
   const originalCollateralRatio = !original.isEmpty ? original.collateralRatio(price) : undefined;
   const collateralRatio = !edited.isEmpty ? edited.collateralRatio(price) : undefined;
-  const collateralRatioChange = Difference.between(collateralRatio, originalCollateralRatio);
+  const collateralRatioPct = new Percent(collateralRatio ?? { toString: () => "N/A" });
+  const originalCollateralRatioPct = new Percent(
+    originalCollateralRatio ?? { toString: () => "N/A" }
+  );
 
   const maxEth = accountBalance.gt(gasRoomETH) ? accountBalance.sub(gasRoomETH) : Decimal.ZERO;
-  const maxCollateral = original.collateral.add(maxEth);
-  const collateralMaxedOut = edited.collateral.eq(maxCollateral);
-
-  const dirty = !edited.equals(original);
 
   return (
-    <Card>
-      <Heading>
-        Trove
-        {dirty && !changePending && (
-          <Button
-            variant="titleIcon"
-            sx={{ ":enabled:hover": { color: "danger" } }}
-            onClick={() => dispatch({ type: "revert" })}
-          >
-            <Icon name="history" size="lg" />
-          </Button>
-        )}
-      </Heading>
+    <div className={classes.wrapper}>
+      <Input
+        label="deposit"
+        placeholder={Decimal.from(deposit || 0).prettify(4)}
+        unit={ETH}
+        value={deposit}
+        onChange={v => {
+          setDeposit(v);
+          dispatch({ type: "setCollateral", newValue: v });
+        }}
+        available={`Wallet ${maxEth.prettify(2)}`}
+        icon={process.env.PUBLIC_URL + "/icons/ethereum-eth.svg"}
+        maxAmount={maxEth.toString()}
+        maxedOut={maxEth.toString() === deposit.toString()}
+        min={0}
+        step={0.1}
+      />
 
-      <Box sx={{ p: [2, 3] }}>
-        <EditableRow
-          label="Collateral"
-          inputId="trove-collateral"
-          amount={edited.collateral.prettify(4)}
-          maxAmount={maxCollateral.toString()}
-          maxedOut={collateralMaxedOut}
-          unit="ETH"
-          {...{ editingState }}
-          editedAmount={edited.collateral.toString(4)}
-          setEditedAmount={editedCollateral =>
-            dispatch({ type: "setCollateral", newValue: editedCollateral })
-          }
-        />
+      <Input
+        label="borrow"
+        placeholder={Decimal.from(borrow || 0).prettify(4)}
+        unit={COIN}
+        value={borrow}
+        onChange={v => {
+          setBorrow(v);
+          dispatch({ type: "setDebt", newValue: v });
+        }}
+        icon={process.env.PUBLIC_URL + "/icons/128-lusd-icon.svg"}
+        min={0}
+        step={100}
+      />
 
-        <EditableRow
-          label="Debt"
-          inputId="trove-debt"
-          amount={edited.debt.prettify()}
-          unit={COIN}
-          {...{ editingState }}
-          editedAmount={edited.debt.toString(2)}
-          setEditedAmount={editedDebt => dispatch({ type: "setDebt", newValue: editedDebt })}
-        />
+      {children}
 
-        {original.isEmpty && (
-          <StaticRow
-            label="Liquidation Reserve"
-            inputId="trove-liquidation-reserve"
-            amount={`${LUSD_LIQUIDATION_RESERVE}`}
-            unit={COIN}
-            infoIcon={
-              <InfoIcon
-                tooltip={
-                  <Card variant="tooltip" sx={{ width: "200px" }}>
-                    An amount set aside to cover the liquidator’s gas costs if your Trove needs to be
-                    liquidated. The amount increases your debt and is refunded if you close your
-                    Trove by fully paying off its net debt.
-                  </Card>
-                }
-              />
-            }
-          />
-        )}
-
-        <StaticRow
-          label="Borrowing Fee"
-          inputId="trove-borrowing-fee"
-          amount={fee.toString(2)}
-          pendingAmount={feePct.toString(2)}
-          unit={COIN}
-          infoIcon={
-            <InfoIcon
-              tooltip={
-                <Card variant="tooltip" sx={{ width: "240px" }}>
-                  This amount is deducted from the borrowed amount as a one-time fee. There are no
-                  recurring fees for borrowing, which is thus interest-free.
-                </Card>
-              }
+      {(deposit > 0 || borrow > 0) && (
+        <div className={classes.statickInfo}>
+          {deposit > 0 && (
+            <StaticRow
+              className={classes.staticRowInfo}
+              label="Deposit"
+              inputId="trove-collateral-value"
+              amount={edited.collateral.prettify(4)}
+              unit={ETH}
             />
-          }
-        />
+          )}
 
-        {children}
-      </Box>
+          <StaticRow
+            className={classes.staticRowInfo}
+            label="Collateral ratio"
+            amount={collateralRatioPct.prettify()}
+            color={getColor(collateralRatio)}
+            oldAmount={originalCollateralRatio && originalCollateralRatioPct.prettify()}
+            oldColor={getColor(originalCollateralRatio)}
+          />
+
+          <StaticRow
+            className={classes.staticRowInfo}
+            label="Borrowing Fee"
+            amount={fee.toString(2)}
+            pendingAmount={feePct.toString(2)}
+            unit={COIN}
+          />
+
+          {original.isEmpty && (
+            <StaticRow
+              className={classes.staticRowInfo}
+              label="Liquidation Reserve"
+              amount={`${LUSD_LIQUIDATION_RESERVE}`}
+              unit={COIN}
+            />
+          )}
+
+          {borrow && (
+            <StaticRow
+              className={classes.staticRowInfo}
+              label="Recieve"
+              inputId="trove-recieve-value"
+              amount={Decimal.from(borrow).prettify(2)}
+            />
+          )}
+        </div>
+      )}
 
       {changePending && <LoadingOverlay />}
-    </Card>
+    </div>
   );
 };
 
-export default TroveEditor;
+export const TroveWithdraw = ({
+  children,
+  original,
+  edited,
+  fee,
+  borrowingRate,
+  changePending,
+  dispatch
+}) => {
+  const { price } = useLiquitySelector(select);
+  const [withdraw, setWithdraw] = useState("");
+  const [repay, setRepay] = useState("");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=ethereum,liquity-usd&vs_currencies=usd&include_24hr_change=true",
+      {
+        method: "GET"
+      }
+    )
+      .then(res => res.json())
+      .then(setData)
+      .catch(console.warn);
+  }, []);
+
+  const originalCollateralRatio = !original.isEmpty ? original.collateralRatio(price) : undefined;
+  const collateralRatio = !edited.isEmpty ? edited.collateralRatio(price) : undefined;
+
+  const collateralRatioPct = new Percent(collateralRatio ?? { toString: () => "N/A" });
+  const originalCollateralRatioPct = new Percent(
+    originalCollateralRatio ?? { toString: () => "N/A" }
+  );
+
+  const maxRepay = original.debt.sub(Decimal.from(LUSD_MINIMUM_DEBT));
+
+  let maxWithdraw = null;
+
+  if (data) {
+    const ETHEREUM_IN_USD = data.ethereum.usd;
+    const LUSD_IN_USD = data["liquity-usd"].usd;
+
+    const ethereumInLusd = ETHEREUM_IN_USD / LUSD_IN_USD;
+
+    maxWithdraw = original.collateral.sub(
+      edited.debt.mul(Decimal.from(1.1).div(Decimal.from(ethereumInLusd)))
+    );
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <Input
+        label="withdraw"
+        placeholder={Decimal.from(withdraw || 0).prettify(4)}
+        unit={ETH}
+        value={withdraw}
+        onChange={v => {
+          setWithdraw(v);
+          dispatch({ type: "substractCollateral", newValue: v });
+        }}
+        available={`Wallet ${maxWithdraw?.prettify(2)}`}
+        icon={process.env.PUBLIC_URL + "/icons/ethereum-eth.svg"}
+        maxAmount={maxWithdraw?.toString()}
+        maxedOut={maxWithdraw?.toString() === withdraw.toString()}
+        min={0}
+        step={0.1}
+      />
+
+      <Input
+        label="repay"
+        placeholder={Decimal.from(repay || 0).prettify(4)}
+        unit={COIN}
+        value={repay}
+        onChange={v => {
+          setRepay(v);
+          dispatch({ type: "substractDebt", newValue: v });
+        }}
+        available={`Available ${maxRepay.prettify(2)}`}
+        icon={process.env.PUBLIC_URL + "/icons/128-lusd-icon.svg"}
+        maxAmount={maxRepay.toString()}
+        maxedOut={maxRepay.toString() === repay.toString()}
+        min={0}
+        step={100}
+      />
+
+      {children}
+
+      {(withdraw > 0 || repay > 0) && (
+        <div className={classes.statickInfo}>
+          {withdraw > 0 && (
+            <StaticRow
+              label="Withdraw"
+              inputId="trove-collateral-value"
+              amount={Decimal.from(withdraw).prettify()}
+              unit={ETH}
+            />
+          )}
+
+          {repay > 0 && (
+            <StaticRow
+              label="repay"
+              inputId="trove-repay-value"
+              amount={Decimal.from(repay).prettify()}
+              unit={COIN}
+            />
+          )}
+
+          <StaticRow
+            label="Collateral ratio"
+            inputId="trove-collateral-ratio"
+            amount={collateralRatioPct.prettify()}
+            color={getColor(collateralRatio)}
+            oldAmount={originalCollateralRatio && originalCollateralRatioPct.prettify()}
+            oldColor={getColor(originalCollateralRatio)}
+          />
+        </div>
+      )}
+
+      {changePending && <LoadingOverlay />}
+    </div>
+  );
+};
