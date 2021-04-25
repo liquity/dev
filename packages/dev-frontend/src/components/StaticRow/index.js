ece43c907e0abc1a1c5ec73fdf02cd605c30fbf1
lukabajic <lukabajic.dev@gmail.com>
2021-04-25 21:50:18 +0200

    Add StaticRow component

---
+++ packages/dev-frontend/src/components/StaticRow/index.js
@@ -0,0 +1,15 @@
+import cn from "classnames";
+
+import classes from "./StaticRow.module.css";
+
+const StaticRow = ({ label, amount, color, className, unit, oldAmount, oldColor }) => (
+  <div className={cn(classes.staticRow, className)}>
+    <p className={classes.staticRowlabel}>{label}</p>
+    <p className={cn(classes.staticRowAmount, classes[color])}>
+      {oldAmount && <span className={classes[oldColor]}>{oldAmount}</span>}
+      {oldAmount && <span> &#8594;</span>} {amount} {unit}
+    </p>
+  </div>
+);
+
+export default StaticRow;
