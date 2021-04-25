3d9473b0f950e2872fa0897d1043d9ceb77ec46b
lukabajic <lukabajic.dev@gmail.com>
2021-04-25 07:18:00 +0200

    Adjust loader and add spinner component

--- packages/dev-frontend/src/components/Loader/index.js
+++ packages/dev-frontend/src/components/Loader/index.js
@@ -1,5 +1,9 @@
 import classes from "./Loader.module.css";
 
+export const Spinner = ({ size = 9 }) => (
+  <div className={classes.loader} style={{ fontSize: size + "rem" }}></div>
+);
+
 const Loader = () => (
   <div className={classes.overlay}>
     <div className={classes.loader}></div>
