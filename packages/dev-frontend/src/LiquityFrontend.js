8dec6ab5281c646608c0b33b2dec4b6e828d62af
lukabajic <lukabajic.dev@gmail.com>
2021-04-25 15:44:26 +0200

    Changes to main container

--- packages/dev-frontend/src/LiquityFrontend.js
+++ packages/dev-frontend/src/LiquityFrontend.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import { Wallet } from "@ethersproject/wallet";
 
@@ -7,7 +7,7 @@ import { LiquityStoreProvider } from "@liquity/lib-react";
 
 import { useLiquity } from "./hooks/LiquityContext";
 import { TransactionMonitor } from "./components/Transaction";
-import { UserAccount } from "./components/UserAccount";
+import { shortenAddress } from "./utils/shortenAddress";
 
 import { PageSwitcher } from "./pages/PageSwitcher";
 
@@ -18,11 +18,27 @@ import { FarmViewProvider } from "./components/Farm/context/FarmViewProvider";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import KickbackRate from "./components/KickbackRate";
+import SystemStats from "./components/SystemStats";
+import PriceMenager from "./components/PriceMenager";
+import Footer from "./components/Footer";
+import UserAccount from "./components/UserAccount";
+import Modal from "./components/Modal";
+import Link from "./components/Link";
+import CopyToClipboard from "./components/CopyToClipboard";
+import Button from "./components/Button";
+
+import classes from "./LiquityFrontend.module.css";
+
+const loader = () => <Loader />;
+
+const detectMetaMask = () => window.ethereum?.isMetaMask ?? false;
 
 export const LiquityFrontend = () => {
+  const [walletModal, setWalletModal] = useState(null);
   const { account, provider, liquity } = useLiquity();
+  const isMetaMask = detectMetaMask();
 
-  // For console tinkering ;-)
   Object.assign(window, {
     account,
     provider,
@@ -34,30 +50,76 @@ export const LiquityFrontend = () => {
   });
 
   return (
-    <LiquityStoreProvider loader={Loader} store={liquity.store}>
+    <LiquityStoreProvider lodaer={loader} store={liquity.store}>
       <TroveViewProvider>
         <StabilityViewProvider>
           <StakingViewProvider>
             <FarmViewProvider>
+              {walletModal && (
+                <Modal title={shortenAddress(account)} onClose={() => setWalletModal(null)}>
+                  <div className={classes.do}>
+                    <CopyToClipboard className={classes.doButton} text={account}>
+                      <span>copy address</span>
+                      <ion-icon name="copy-outline"></ion-icon>
+                    </CopyToClipboard>
+                    <Link
+                      className={classes.doButton}
+                      href={`https://kovan.etherscan.io/address/${account}`}
+                    >
+                      <span>view on Etherscan</span>
+                      <ion-icon name="open-outline"></ion-icon>
+                    </Link>
+                  </div>
+                  <p className={classes.connection}>
+                    {isMetaMask ? "Connected with MetaMask" : "Connected with wallet"}{" "}
+                    {isMetaMask && (
+                      <img
+                        className={classes.metaMaskIcon}
+                        src={`${process.env.PUBLIC_URL}/icons/metamask.png`}
+                        alt="MetaMask"
+                      />
+                    )}
+                  </p>
+                  <div className={classes.actions}>
+                    <Button className={classes.fourthButton}>change</Button>
+                    <Button
+                      className={classes.fourthButton}
+                      onClick={() => window.ethereum._handleDisconnect()}
+                    >
+                      disconnect
+                    </Button>
+                  </div>
+                </Modal>
+              )}
+
               <Header>
-                <UserAccount />
+                <UserAccount onWalletClick={() => setWalletModal(true)} />
               </Header>
+
+              <KickbackRate />
+              <SystemStats />
+              <PriceMenager />
 
               <Body>
                 <Switch>
                   <Route path="/" exact>
                     <PageSwitcher />
                   </Route>
+
                   <Route path="/stability-pool">
                     <div>Stability Pool</div>
                   </Route>
+
                   <Route path="/stake">
                     <div>Stake</div>
                   </Route>
+
                   <Route path="/liquidation">
                     <div>liquidation</div>
                   </Route>
                 </Switch>
+
+                <Footer />
               </Body>
             </FarmViewProvider>
           </StakingViewProvider>
