005d0bac902e5cc0f4632c0bfb4325075455865c
lukabajic <lukabajic.dev@gmail.com>
2021-04-25 07:18:14 +0200

    Create Modal

--- packages/dev-frontend/src/components/Modal.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Container } from "theme-ui";
-
-export const Modal: React.FC = ({ children }) => (
-  <Container variant="modalOverlay">
-    <Container variant="modal">{children}</Container>
-  </Container>
-);
--- packages/dev-frontend/src/components/Modal/index.js
+++ packages/dev-frontend/src/components/Modal/index.js
@@ -1,10 +1,63 @@
+import cn from "classnames";
+
+import Button from "../Button";
+
 import classes from "./Modal.module.css";
 
-const Modal = ({ title, content, actions = [], children, onClose }) => (
+const Modal = ({ title, content, confirm, decline, children, onClose, status }) => (
   <div className={classes.overlay} onClick={onClose}>
     <div className={classes.wrapper} onClick={e => e.stopPropagation()}>
+      {status && (
+        <div className={classes.status}>
+          <div
+            className={cn(classes.statusIcon, {
+              [classes.success]: status === "success",
+              [classes.warning]: status === "warning",
+              [classes.error]: status === "error"
+            })}
+          >
+            {status === "success" ? (
+              <ion-icon name="checkmark-outline"></ion-icon>
+            ) : (
+              <ion-icon name="alert-outline"></ion-icon>
+            )}
+          </div>
+        </div>
+      )}
+
       <h3 className={classes.title}>{title}</h3>
-      {children}
+
+      {content && <div className={classes.content}>{content}</div>}
+
+      <div className={classes.children}>{children}</div>
+
+      {(confirm || decline) && (
+        <div className={classes.actions}>
+          {decline && (
+            <Button
+              medium
+              tertiary
+              onClick={() => {
+                onClose && onClose();
+              }}
+            >
+              {decline.text}
+            </Button>
+          )}
+
+          {confirm && (
+            <Button
+              primary
+              medium
+              onClick={() => {
+                onClose && onClose();
+              }}
+            >
+              {confirm.text}
+            </Button>
+          )}
+        </div>
+      )}
     </div>
   </div>
 );
