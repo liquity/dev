5c512474d9dc1246398eda1d1edc13ea3088c5b0
lukabajic <lukabajic.dev@gmail.com>
2021-04-25 09:50:42 +0200

    Convert more files to JS

--- packages/dev-frontend/src/pages/PageSwitcher.js
+++ packages/dev-frontend/src/pages/PageSwitcher.js
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
 import { AddressZero } from "@ethersproject/constants";
 
-import { LiquityStoreState } from "@liquity/lib-base";
 import { useLiquitySelector } from "@liquity/lib-react";
 
 import { useLiquity } from "../hooks/LiquityContext";
 
-import { Dashboard } from "./Dashboard";
 import { UnregisteredFrontend } from "./UnregisteredFrontend";
 import { FrontendRegistration } from "./FrontendRegistration";
 import { FrontendRegistrationSuccess } from "./FrontendRegistrationSuccess";
 
-const selectFrontend = ({ frontend }: LiquityStoreState) => frontend;
+const selectFrontend = ({ frontend }) => frontend;
 
-export const PageSwitcher: React.FC = () => {
+export const PageSwitcher = () => {
   const {
     account,
     config: { frontendTag }
@@ -39,6 +37,6 @@ export const PageSwitcher: React.FC = () => {
       return <UnregisteredFrontend />;
     }
   } else {
-    return <Dashboard />;
+    return <div>Trove</div>;
   }
 };
