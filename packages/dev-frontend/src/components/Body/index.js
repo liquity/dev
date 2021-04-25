38a9c6fca7db4fb3cad63a075e9b4830cd4be9e8
lukabajic <lukabajic.dev@gmail.com>
2021-04-25 15:42:48 +0200

    Add background image

--- packages/dev-frontend/src/components/Body/index.js
+++ packages/dev-frontend/src/components/Body/index.js
@@ -1,5 +1,13 @@
 import classes from "./Body.module.css";
 
-const Body = ({ children }) => <div className={classes.wrapper}>{children}</div>;
+const Body = ({ children }) => (
+  <div className={classes.wrapper}>
+    <div
+      className={classes.background}
+      style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/background.jpg)` }}
+    />
+    {children}
+  </div>
+);
 
 export default Body;
