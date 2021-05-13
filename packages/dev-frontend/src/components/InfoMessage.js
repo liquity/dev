fe4b9082035c47e25e03d00b1a85aefe8d86d80f
lukabajic <lukabajic.dev@gmail.com>
2021-05-13 17:21:25 +0200

    Convert to JS

--- packages/dev-frontend/src/components/InfoMessage.js
+++ packages/dev-frontend/src/components/InfoMessage.js
@@ -2,12 +2,7 @@ import { Box, Flex, Heading, Text } from "theme-ui";
 
 import { Icon } from "./Icon";
 
-type InfoMessageProps = {
-  title: string;
-  icon?: React.ReactNode;
-};
-
-export const InfoMessage: React.FC<InfoMessageProps> = ({ title, children, icon }) => (
+export const InfoMessage = ({ title, children, icon }) => (
   <Box sx={{ mx: 1, mb: 3 }}>
     <Flex sx={{ alignItems: "center", mb: "10px" }}>
       <Box sx={{ mr: "12px", fontSize: "20px" }}>{icon || <Icon name="info-circle" />}</Box>
