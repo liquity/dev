b9477342ec5c95b37f3b0f09aaca105c1254351d
lukabajic <lukabajic.dev@gmail.com>
2021-05-13 16:40:20 +0200

    Convert loading overlay to JS

---
+++ packages/dev-frontend/src/components/LoadingOverlay.js
@@ -0,0 +1,12 @@
+export const LoadingOverlay = () => (
+  <div
+    style={{
+      position: "fixed",
+      top: 0,
+      left: 0,
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: "rgba(255, 255, 255, 0.75)"
+    }}
+  />
+);
