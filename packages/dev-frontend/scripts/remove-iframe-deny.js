2151ba27eb8ca6cd5942c9585bf1fae0fa0efec2
shmuel <shmuel@shmuels-MacBook-Pro.local>
2021-08-04 12:53:15 +0300

    cf-build  script

---
+++ packages/dev-frontend/scripts/remove-iframe-deny.js
@@ -0,0 +1,9 @@
+const { execSync } = require("child_process");
+const { existsSync, readFileSync, writeFileSync } = require("fs");
+
+const workerFile = "workers-site/index.js"
+
+const workerScript = readFileSync(workerFile, { encoding: "ascii" })
+  .replace(`response.headers.set('X-Frame-Options', 'DENY')`, "");
+
+writeFileSync(workerFile, workerScript);
