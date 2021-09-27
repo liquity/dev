8f52f2906f99414c0b1c3a84c95c74c319b7a8c6
ßingen <bingen@liquity.org>
2021-09-27 17:39:48 +0200

    contracts: Set test network hardfork to istanbul...
    ...until unit tests are fixed.

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -68,6 +68,7 @@ module.exports = {
             gas: 10000000,  // tx gas limit
             blockGasLimit: 12500000, 
             gasPrice: 20000000000,
+            hardfork: "istanbul", // TODO: fix after unit tests are fixed!
         },
         mainnet: {
             url: alchemyUrl(),
