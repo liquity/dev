8b978c9da7c46c9df3f9296063a43def8076d468
xPi2 <uxiopineiro@gmail.com>
2022-03-10 22:32:35 +0000

    style: clean user profile part of the nav #30

--- packages/dev-frontend/src/components/UserAccount.tsx
+++ packages/dev-frontend/src/components/UserAccount.tsx
@@ -7,45 +7,48 @@ import { useLiquitySelector } from "@liquity/lib-react";
 import { COIN, GT } from "../strings";
 import { useLiquity } from "../hooks/LiquityContext";
 import { shortenAddress } from "../utils/shortenAddress";
+import { Badge } from "../components/Badge";
 
 import { Icon } from "./Icon";
 
 const select = ({ accountBalance, lusdBalance, lqtyBalance }: LiquityStoreState) => ({
-  accountBalance,
-  lusdBalance,
-  lqtyBalance
+    accountBalance,
+    lusdBalance,
+    lqtyBalance
 });
 
 export const UserAccount: React.FC = () => {
-  const { account } = useLiquity();
-  const { accountBalance, lusdBalance, lqtyBalance } = useLiquitySelector(select);
+    const { account } = useLiquity();
+    const { accountBalance, lusdBalance, lqtyBalance } = useLiquitySelector(select);
 
-  return (
-    <Box sx={{ display: ["none", "flex"] }}>
-      <Flex sx={{ alignItems: "center" }}>
-        <Icon name="user-circle" size="lg" />
-        <Flex sx={{ ml: 3, mr: 4, flexDirection: "column" }}>
-          <Heading sx={{ fontSize: 1 }}>Connected as</Heading>
-          <Text as="span" sx={{ fontSize: 1 }}>
-            {shortenAddress(account)}
-          </Text>
-        </Flex>
-      </Flex>
+    return (
+        <Box sx={{ display: ["none", "flex"] }}>
+            <Flex sx={{ mx: 2, alignItems: "center" }}>
+                {/* <Icon name="user-circle" size="lg" /> */}
+                <Badge>
+                    <Flex sx={{ p: 1, flexDirection: "column" }}>
+                        {/* <Heading sx={{ fontSize: 1 }}>Connected as</Heading> */}
+                        <Text as="span" sx={{ fontSize: 1 }}>
+                            {shortenAddress(account)}
+                        </Text>
+                    </Flex>
+                </Badge>
+            </Flex>
 
-      <Flex sx={{ alignItems: "center" }}>
-        <Icon name="wallet" size="lg" />
+            <Flex sx={{ alignItems: "center" }}>
+                {/* <Icon name="wallet" size="lg" /> */}
 
-        {([
-          ["ETH", accountBalance],
-          [COIN, lusdBalance],
-          [GT, lqtyBalance]
-        ] as const).map(([currency, balance], i) => (
-          <Flex key={i} sx={{ ml: 3, flexDirection: "column" }}>
-            <Heading sx={{ fontSize: 1 }}>{currency}</Heading>
-            <Text sx={{ fontSize: 1 }}>{balance.prettify()}</Text>
-          </Flex>
-        ))}
-      </Flex>
-    </Box>
-  );
+                {([
+                    ["ETH", accountBalance],
+                    [COIN, lusdBalance],
+                    [GT, lqtyBalance]
+                ] as const).map(([currency, balance], i) => (
+                    <Flex key={i} sx={{ ml: 3, flexDirection: "column" }}>
+                        <Heading sx={{ fontSize: 1 }}>{currency}</Heading>
+                        <Text sx={{ fontSize: 1 }}>{balance.prettify()}</Text>
+                    </Flex>
+                ))}
+            </Flex>
+        </Box>
+    );
 };
