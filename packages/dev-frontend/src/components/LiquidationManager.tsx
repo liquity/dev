57140d18b1498c1cfa1d60fbde179bf88f4435ad
xPi2 <uxiopineiro@gmail.com>
2022-03-23 21:53:14 +0000

    style: rework liquidation button #26

--- packages/dev-frontend/src/components/LiquidationManager.tsx
+++ packages/dev-frontend/src/components/LiquidationManager.tsx
@@ -7,48 +7,47 @@ import { Icon } from "./Icon";
 import { Transaction } from "./Transaction";
 
 export const LiquidationManager: React.FC = () => {
-  const {
-    liquity: { send: liquity }
-  } = useLiquity();
-  const [numberOfTrovesToLiquidate, setNumberOfTrovesToLiquidate] = useState("90");
+    const {
+        liquity: { send: liquity }
+    } = useLiquity();
+    const [numberOfTrovesToLiquidate, setNumberOfTrovesToLiquidate] = useState("90");
 
-  return (
-    <Card>
-      <Heading>Liquidate</Heading>
+    return (
+        <Card>
+            <Box sx={{ p: [2, 3] }}>
+                <Flex sx={{ alignItems: "stretch" }}>
+                    <Flex sx={{ ml: 2, alignItems: "center" }}>
+                        <Transaction
+                            id="batch-liquidate"
+                            tooltip="Liquidate"
+                            tooltipPlacement="bottom"
+                            send={overrides => {
+                                if (!numberOfTrovesToLiquidate) {
+                                    throw new Error("Invalid number");
+                                }
+                                return liquity.liquidateUpTo(parseInt(numberOfTrovesToLiquidate, 10), overrides);
+                            }}
+                        >
+                            <Button variant="danger">
+                                Liquidate
+                            </Button>
+                        </Transaction>
+                        <Label>Up to</Label>
 
-      <Box sx={{ p: [2, 3] }}>
-        <Flex sx={{ alignItems: "stretch" }}>
-          <Label>Up to</Label>
+                        <Input
+                            type="number"
+                            min="1"
+                            step="1"
+                            value={numberOfTrovesToLiquidate}
+                            onChange={e => setNumberOfTrovesToLiquidate(e.target.value)}
+                        />
 
-          <Input
-            type="number"
-            min="1"
-            step="1"
-            value={numberOfTrovesToLiquidate}
-            onChange={e => setNumberOfTrovesToLiquidate(e.target.value)}
-          />
+                        <Label>Troves</Label>
 
-          <Label>Troves</Label>
 
-          <Flex sx={{ ml: 2, alignItems: "center" }}>
-            <Transaction
-              id="batch-liquidate"
-              tooltip="Liquidate"
-              tooltipPlacement="bottom"
-              send={overrides => {
-                if (!numberOfTrovesToLiquidate) {
-                  throw new Error("Invalid number");
-                }
-                return liquity.liquidateUpTo(parseInt(numberOfTrovesToLiquidate, 10), overrides);
-              }}
-            >
-              <Button variant="dangerIcon">
-                <Icon name="trash" size="lg" />
-              </Button>
-            </Transaction>
-          </Flex>
-        </Flex>
-      </Box>
-    </Card>
-  );
+                    </Flex>
+                </Flex>
+            </Box>
+        </Card>
+    );
 };
