7414eda442206165a787cb3d605528801ae26e4e
Edward Mulraney <edmulraney@gmail.com>
2022-08-19 16:22:59 +0100

    Add LUSD faucet button

--- packages/dev-frontend/src/components/Bonds/views/idle/Idle.tsx
+++ packages/dev-frontend/src/components/Bonds/views/idle/Idle.tsx
@@ -5,13 +5,26 @@ import { Bonds } from "./Bonds";
 import { useBondView } from "../../context/BondViewContext";
 import { BONDS } from "../../lexicon";
 import { InfoIcon } from "../../../InfoIcon";
+import { LUSD_OVERRIDE_ADDRESS } from "@liquity/chicken-bonds/lusd/addresses";
 
 export const Idle: React.FC = () => {
-  const { dispatchEvent, bonds, isSynchronising } = useBondView();
+  const { dispatchEvent, bonds, isSynchronising, getLusdFromFaucet, lusdBalance } = useBondView();
+
+  if (lusdBalance === undefined) return null;
+
   const hasBonds = bonds !== undefined && bonds.length > 0;
+
+  const showLusdFaucet = LUSD_OVERRIDE_ADDRESS !== null && lusdBalance.eq(0);
 
   return (
     <>
+      {showLusdFaucet && (
+        <Flex variant="layout.actions" mt={4}>
+          <Button variant="outline" onClick={() => getLusdFromFaucet()}>
+            Get 10k LUSD
+          </Button>
+        </Flex>
+      )}
       {(hasBonds || isSynchronising) && (
         <>
           <Flex variant="layout.actions" mt={4}>
