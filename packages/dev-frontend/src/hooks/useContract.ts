4d9c3140a68bf100e3d314c923872d622ec1f038
Edward Mulraney <edmulraney@gmail.com>
2022-09-28 13:16:27 +0100

    Catch error from contracts that aren't deployed

--- packages/dev-frontend/src/hooks/useContract.ts
+++ packages/dev-frontend/src/hooks/useContract.ts
@@ -17,6 +17,9 @@ export function useContract<TContractType>(
       try {
         if (contract !== undefined || address === null) return;
 
+        const exists = (await provider.getCode(address)) !== "0x";
+        if (!exists) throw new Error(`Contract ${address} doesn't exist.`);
+
         const connectedContract = (new ethers.Contract(
           address,
           abi,
