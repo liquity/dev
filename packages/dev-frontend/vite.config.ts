3063ed72111d2e54ddb2a0102b4707064635bb31
Daniel Simon <daniel.attila.simon@gmail.com>
2023-06-13 19:08:00 +0700

    fix: 404 when put in a subdirectory

--- packages/dev-frontend/vite.config.ts
+++ packages/dev-frontend/vite.config.ts
@@ -7,6 +7,7 @@ import RollupPluginPolyfillNode from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   optimizeDeps: {
     include: [
