84f94df512d14b7451aedf8079923137d02c8fd9
Daniel Simon <daniel.attila.simon@gmail.com>
2023-06-13 17:02:47 +0700

    feat: expose new config options in Docker app

--- packages/dev-frontend/docker-entrypoint.d/99-frontend-config.sh
+++ packages/dev-frontend/docker-entrypoint.d/99-frontend-config.sh
@@ -4,7 +4,9 @@ echo_config() {
   echo '{'
   [ -n "$TESTNET_ONLY" ] && echo '  "testnetOnly": '$TESTNET_ONLY','
   echo '  "frontendTag": "'$FRONTEND_TAG'",'
-  echo '  "infuraApiKey": "'$INFURA_API_KEY'"'
+  echo '  "infuraApiKey": "'$INFURA_API_KEY'",'
+  echo '  "alchemyApiKey": "'$ALCHEMY_API_KEY'",'
+  echo '  "walletConnectProjectId": "'$WALLET_CONNECT_PROJECT_ID'"'
   echo '}'
 }
 
