574566c6bf4ddaf002b6a168881ae2b2e6582d17
evandrosaturnino <evandro.saturnino@gmail.com>
2023-07-25 22:44:26 -0300

    refactor: gas limit changed in test

--- packages/lib-ethers/test/Stabilio.test.ts
+++ packages/lib-ethers/test/Stabilio.test.ts
@@ -1175,7 +1175,7 @@ describe("EthersStabilio", () => {
       // Required gas has just went up.
       const newGasEstimate = await provider.estimateGas(tx.rawPopulatedTransaction);
       const gasIncrease = newGasEstimate.sub(originalGasEstimate).toNumber();
-      expect(gasIncrease).to.be.within(5000, 10000);
+      expect(gasIncrease).to.be.within(4500, 10000);
 
       // This will now have to update lastFeeOperationTime
       await waitForSuccess(tx.send());
