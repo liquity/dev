e8c1f70519a2d2ee9945b0a1573dd830809abe4a
evandrosaturnino <evandro.saturnino@gmail.com>
2023-08-02 00:00:16 -0300

    layout: nav gap addition

--- packages/dev-frontend/src/components/Nav.tsx
+++ packages/dev-frontend/src/components/Nav.tsx
@@ -4,11 +4,11 @@ import { Link } from "./Link";
 export const Nav: React.FC = () => {
   return (
     <Box as="nav" sx={{ display: ["none", "flex"], alignItems: "center", flex: 1 }}>
-      <Flex>
+      <Flex sx={{ gap: 3 }}>
         <Link to="/">Dashboard</Link>
         <Link to="/farm">Farm</Link>
       </Flex>
-      <Flex sx={{ justifyContent: "flex-end", mr: 3, flex: 1 }}>
+      <Flex sx={{ justifyContent: "flex-end", mr: 3, flex: 1, gap: 3 }}>
         <Link to="/risky-troves">
           Risky Troves
         </Link>
